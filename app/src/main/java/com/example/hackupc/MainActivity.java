086d03a12a57b13e1000ba2165f7844684c36ae3
Latra <personadelmonton@gmail.com>
2019-10-12 05:51:35 +0200

    Modificación aspecto interfaz + JSON dummy de Center Point

--- app/src/main/java/com/example/hackupc/MainActivity.java
+++ app/src/main/java/com/example/hackupc/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.hackupc;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Paint;
+import android.graphics.Point;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -17,6 +19,10 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
     public static final int CAMERA_REQUEST = 9999;
     ImageView imageView;
@@ -24,8 +30,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
         imageView = (ImageView)findViewById(R.id.imageView);
     }
 
@@ -62,6 +66,81 @@ public class MainActivity extends AppCompatActivity {
         if(requestCode==CAMERA_REQUEST){
            Bitmap bitmap= (Bitmap) data.getExtras().get("data");
            imageView.setImageBitmap(bitmap);
+           //send image to API
+            //get result
+            JSONObject result = null;
+            JSONArray resultArray = null;
+            try {
+                result = imageResult();
+                result = result.getJSONObject("response");
+                result = result.getJSONObject("solutions");
+                result = result.getJSONObject("re_features_v3");
+                resultArray = result.getJSONArray("detections");
+                int length = resultArray.length();
+                JSONObject tempPoint = null;
+                JSONObject temp = null;
+                String[][] resultData = new String[length][3];
+                for (int i = 0; i < length ; i++) {
+                    temp = (JSONObject) resultArray.get(i);
+                    tempPoint = temp.getJSONObject("center_point");
+                    resultData[i][0] = temp.getString("label");
+                    resultData[i][1] = tempPoint.getString("y");
+                    resultData[i][2] = tempPoint.getString("x");
+                }
+
+            } catch (JSONException e) {
+
+            }
+            //print cuadrados
         }
     }
+
+// devuelve dummy para pruebas
+    public JSONObject imageResult(){
+        try {
+            return new JSONObject(
+                    "{" +
+                         "   \"error\": \"false\","+
+                    "\"correlation_id\": \"4e97bcbb-53af-4528-a22f-d46f2ce23db6\","+
+                    "\"version\": \"2\","+
+                    "\"time\": \"2018-10-02T13:09:40.448881\","+
+                    "\"response\": {"+
+                "\"solutions\": {"+
+                    "\"re_features_v3\": {" +
+                        "\"detections\": [ " +
+                        "{" +
+"                            \"center_point\": {"+
+                            "\"y\": 0.8842905405405406,"+
+                         "           \"x\": 0.35208098987626546"+
+                        "},"+
+                            "\"label\": \"hardwood_floor\""+
+                        "},"+
+                        "{"+
+                            "\"center_point\": { "+
+                            "\"y\": 0.18412162162162163,"+
+                         "           \"x\": 0.5067491563554556"+
+                        "},"+
+                            "\"label\": \"coffered_ceiling\""+
+                        "},"+
+                        "{"+
+                            "\"center_point\": {"+
+                           " \"y\": 0.18158783783783783,"+
+                         "           \"x\": 0.5573678290213724"+
+                        "},"+
+                       "     \"label\": \"beamed_ceiling\" "+
+                        "}"+
+                "]"+
+                    "}"+
+                "}"+
+            "}"+
+"}"
+            );
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
 }
+
